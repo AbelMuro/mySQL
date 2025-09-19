8bc82937180e0b45c4d45b8bdc8097a90caa0df2
abelmuro <abelmuro93@gmail.com>
2025-09-19 12:04:02 -0700

    commit

--- src/Config/mySQL/db.js
+++ src/Config/mySQL/db.js
@@ -4,6 +4,7 @@ config();
 
 const db = mysql.createPool({
     host: process.env.host,
+    port: process.env.port,
     user: process.env.user,
     password: process.env.password,
     database: process.env.database,
